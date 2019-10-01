b690d078da7c5833d07c7782dda774fb8853eca8
EzequielGnich <ezequielgnich@live.com>
2019-10-01 11:44:09 -0300

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const port = process.env.PORT || 3333;
+
+// req.query = acessar query params (para filtros)
+// req.params = acessar route params (para edição e delete)
+// req.body = acessar corpo da requisição (para criação e edição)
+
 app.get("/", (req, res) => {
   res.json({ name: "Ola mundo" });
 });
 
-app.listen(3333);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}, 
+  access link http://localhost:${port}`);
+});
