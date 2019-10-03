2e0af7042ebd693fedcb4f0f1d445593470c5e3d
EzequielGnich <ezequielgnich@live.com>
2019-10-03 12:47:14 -0300

    Criado servidor da aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-ux1ju.mongodb.net/omnistack9?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const routes = require("./routes");
 
 const port = process.env.PORT || 3333;
 
@@ -9,9 +18,8 @@ const port = process.env.PORT || 3333;
 // req.params = acessar route params (para edição e delete)
 // req.body = acessar corpo da requisição (para criação e edição)
 
-app.get("/", (req, res) => {
-  res.json({ name: "Ola mundo" });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}, 
