32ef68ddeea2bec5f8e6c1d4e16c95af32b5504a
EzequielGnich <ezequielgnich@live.com>
2019-10-04 09:47:04 -0300

    Criada rotas de Dashboard, Booking e spots

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,10 +8,15 @@ const upload = multer(uploadConfig);
 
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
+const DashboardController = require("./controllers/DashboardController");
+const BookingController = require("./controllers/BookingController");
 
 routes.post("/sessions", SessionController.store);
 
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 routes.get("/spots", SpotController.index);
+routes.post("/spots/:id/booking", BookingController.store);
+
+routes.get("/dashboard", DashboardController.show);
 
 module.exports = routes;
