367fd28b12c5b0a2da49da2dcdb4d2669a0c836f
EzequielGnich <ezequielgnich@live.com>
2019-10-04 09:46:06 -0300

    Criado dashboard controller

--- backend/src/controllers/DashboardController.js
+++ backend/src/controllers/DashboardController.js
@@ -1,3 +1,10 @@
+const Spot = require("../models/SpotModel");
+
 module.exports = {
-  async show(req, res) {}
+  async show(req, res) {
+    const { user_id } = req.headers;
+    const spots = await Spot.find({ user: user_id });
+
+    return res.status(200).send(spots);
+  }
 };
