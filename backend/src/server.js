67ccd1ccbb9fdefed90c1b65246e3b58e101a06a
EzequielGnich <ezequielgnich@live.com>
2019-10-05 17:01:08 -0300

    Aplicação do cors, criada rota para gerar url para thumbanil

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const path = require("path");
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@cluster0-ux1ju.mongodb.net/omnistack9?retryWrites=true&w=majority",
@@ -18,7 +20,10 @@ const port = process.env.PORT || 3333;
 // req.params = acessar route params (para edição e delete)
 // req.body = acessar corpo da requisição (para criação e edição)
 
+app.use(cors());
 app.use(express.json());
+// Usado para enviar a imgaem para o frontend
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(port, () => {
