30f41a2f6e23420ae2dba2b34bd5d4d4a2fda985
EzequielGnich <ezequielgnich@live.com>
2019-10-06 23:26:45 -0300

    Criada a função que escuta qualquer solicitação do frontend

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -3,12 +3,12 @@ const Booking = require("../models/BookingModel");
 module.exports = {
   async store(req, res) {
     const { user_id } = req.headers;
-    const { spot_id } = req.params;
+    const { id } = req.params;
     const { date } = req.body;
 
     const booking = await Booking.create({
       user: user_id,
-      spot: spot_id,
+      spot: id,
       date
     });
 
@@ -19,6 +19,12 @@ module.exports = {
       .populate("user")
       .execPopulate();
 
-    return res.status(200).json(booking);
+    const ownerSocket = req.connectedUsers[booking.spot.user];
+
+    if (ownerSocket) {
+      req.io.to(ownerSocket).emit("booking_request", booking);
+    }
+
+    return res.json(booking);
   }
 };
